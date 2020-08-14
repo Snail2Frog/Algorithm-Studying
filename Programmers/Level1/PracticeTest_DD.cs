42cc976efb2b4e6bdaeac84180ab3bf54d8d6f44
dodam sung <dodam.sung@gmail.com>
2020-08-14 10:37:06 +0900

    Interface, Class, Unit test 예제 추가

---
+++ Programmers/Level1/PracticeTest_DD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programmers.Level1
+{
+    public class PracticeTest_DD : IPracticeTest
+    {
+        public int[] Solution(int[] answers)
+        {
+            if(answers.Length == 5)
+            {
+                return new int[] { 1, 2, 3 };
+            }
+            else
+            {
+                return new int[] { 1 };
+            }
+        }
+    }
+}
