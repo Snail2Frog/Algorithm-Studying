72fec1d9aba28b161be00aed19d71383a9495acb
dodam sung <dodam.sung@gmail.com>
2020-08-26 10:13:44 +0900

    Year2016 Solution 2개 추가(편법, 출제자의도)

--- Programmers/Year2016/Year2016_DD.cs
+++ Programmers/Year2016/Year2016_DD.cs
@@ -1,10 +1,17 @@
-﻿namespace Programmers.Year2016
+﻿using System;
+
+namespace Programmers.Year2016
 {
     public class Year2016_DD : IYear2016
     {
         public string Solution((int a, int b) input)
         {
-            throw new System.NotImplementedException();
+            int a = input.a;
+            int b = input.b; 
+
+            var dateTime = new DateTime(2016, a, b);
+
+            return dateTime.DayOfWeek.ToString().Substring(0,3).ToUpper();
         }
     }
 }
