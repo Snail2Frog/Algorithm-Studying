953427c23d497559ea355e54f5990390a43a7112
dodam sung <dodam.sung@gmail.com>
2020-08-27 09:32:25 +0900

    나누어 떨어지는 숫자 배열 문제, Solution, UnitTest 추가

---
+++ ProgrammersTests/DevidedNumber/DevidedNumberTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammersTests;
+using System.Linq;
+
+namespace Programmers.DevidedNumber.Tests
+{
+    [TestClass()]
+    public class DevidedNumberTests : TestsBase<(int[] arr, int divisor), int[]>
+    {
+        public override bool GetResult(int[] answer, int[] expectedAnswer)
+        {
+            return Enumerable.SequenceEqual(answer, expectedAnswer);
+        }
+
+        public override void Init()
+        {
+            AddSample((new int[] { 5, 7, 9, 10 }, 5), new int[] { 5, 10 });
+            AddSample((new int[] { 2, 31, 1, 3 }, 1), new int[] { 1, 2, 3, 36 });
+            AddSample((new int[] { 3, 2, 6 }, 10), new int[] { -1 });
+        }
+
+        [TestMethod()]
+        [DataRow("DevidedNumber_DD")]
+        public override void Test(string solutionClassName)
+        {
+            SolutionTest(solutionClassName);
+        }
+    }
+}
