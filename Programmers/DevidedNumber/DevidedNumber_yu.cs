5a26c0539a8aea7d4d25f03a85aa6dd824d6b631
yu seungjin <yuwin@dootait.com>
2020-08-27 10:32:30 +0900

    완료

---
+++ Programmers/DevidedNumber/DevidedNumber_yu.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Programmers.DevidedNumber
+{
+    public class DevidedNumber_yu : IDevidedNumber
+    {
+        public int[] Solution((int[] arr, int divisor) input)
+        {
+            int[] arr = input.arr;
+            int divisor = input.divisor;
+
+            int[] answer = new int[] { };
+            
+            var devided = arr.Where(d=>d % divisor == 0).ToList();
+
+            if (devided.Count() > 0)
+            {
+                devided.Sort();
+                answer = devided.ToArray();
+            }
+            else
+            {
+                answer = new int[] { -1 };
+            }
+            return answer;
+        }
+    }
+}
