cb551f6261b9df50a879cdbd058bcb7808e605f5
dodam sung <dodam.sung@gmail.com>
2020-09-01 16:41:25 +0900

    직사각형 별찍기, x만큼 간격이 있는 n개의 숫자, 행렬의 덧셈 추가, Unit test 프로젝트 nuget 버전 다운그레이드(테스트 안되는 문제 발생)

---
+++ ProgrammersTests/SumOfArray/SumOfArrayTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammersTests;
+
+namespace Programmers.SumOfArray.Tests
+{
+    [TestClass()]
+    public class SumOfArrayTests : TestsBase<(int[,] arr1, int[,] arr2), int[,]>
+    {
+        public override bool GetResult(int[,] answer, int[,] expectedAnswer)
+        {
+            int rows    = answer.GetLength(0);
+            int columns = answer.GetLength(1);
+
+            bool result = true;
+            for(int row = 0; row < rows; row++)
+            {
+                for(int column = 0; column < columns; column++)
+                {
+                    if(answer[row, column] != expectedAnswer[row, column])
+                    {
+                        result = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+        [TestInitialize]
+        public override void Init()
+        {
+            AddSample((new int[,] { { 1,2,3 }, { 2,3,4 }, { 2,3,4 }, { 2,3,4 } }, new int[,] { { 3,4,5 }, { 5,6,7 }, { 5,6,7 }, { 5,6,7 } }), new int[,] { { 4,6,8 }, { 7,9,11 }, { 7,9,11 }, { 7,9,11 } });
+            AddSample((new int[,] { { 1,2 }, { 2,3 } }, new int[,] { { 3,4 }, { 5,6 } }), new int[,] { { 4,6 }, { 7,9 } });
+            AddSample((new int[,] { { 1 }, { 2 } }, new int[,] { { 3 }, { 4 } }), new int[,] { { 4 }, { 6 } });
+        }
+
+        [TestMethod]
+        [DataRow("SumOfArray_DD")]
+        public override void Test(string solutionClassName)
+        {
+            SolutionTest(solutionClassName);
+        }
+    }
+}
