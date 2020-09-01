99d41d705258153f13815922c2d6b71087970413
dodam sung <dodam.sung@gmail.com>
2020-09-02 08:30:36 +0900

    핸드폰 번호 가리기 문제, solution, unit test 추가

---
+++ Programmers/HidingPhoneNumber/HidingPhoneNumber_DD.cs
@@ -0,0 +1,17 @@
+﻿namespace Programmers.HidingPhoneNumber
+{
+    public class HidingPhoneNumber_DD : IHidingPhoneNumber
+    {
+        public string Solution(string phone_number)
+        {
+            int totalLength = phone_number.Length;
+            int hidingLength = totalLength - 4;
+
+            string answer = phone_number.Remove(0, hidingLength);
+
+            answer = answer.PadLeft(totalLength, '*');
+
+            return answer;
+        }
+    }
+}
